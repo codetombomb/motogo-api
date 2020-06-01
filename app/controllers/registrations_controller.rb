91d6fdc857a1cc00de48e998c6d46957a89e6456
github username <github email address>
2020-06-01 11:40:02 -0500

    Switched back to sessions

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,27 @@
+class RegistrationsController < ApplicationController
+    def create
+        user = User.create!(
+            first_name: params['user']['first_name'],
+            last_name: params['user']['last_name'],
+            email: params['user']['email'],
+            password: params['user']['password'],
+            password_confirmation: params['user']['password_confirmation'],
+            address: params['user']['address'],
+            city: params['user']['city'],
+            state: params['user']['state'],
+            zip_code: params['user']['zip_code'],
+            phone_number: params['user']['phone_number']
+
+        )
+        if user
+            session[:user_id] = user.id 
+            render json: {
+                status: :created,
+                user: user
+            }
+        else 
+            render json: { status: 500 }
+        end
+    end
+end
+
