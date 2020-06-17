93ce9496c09b62eb7d7ea7b6b7f761e66aa332ab
github username <github email address>
2020-06-17 15:53:14 -0500

    corrected the my info/ bike rental info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,27 +7,35 @@ class UsersController < ApplicationController
   end
 
   def bike_rent_history
-    @rent_history = []
-    my_rented_bikes = []
-    my_renter_posts = []
-    my_posts = []
-    RenterPost.all.map do |rp|
-      if rp.renter_id == @current_user.id
-        my_renter_posts << rp
+    if @current_user
+      @rent_history = []
+      my_rented_bikes = []
+      my_renter_posts = []
+      my_posts = []
+      RenterPost.all.map do |rp|
+        if rp.renter_id == @current_user.id
+          my_renter_posts << rp
+        end
       end
+      my_renter_posts.each do |post|
+        my_posts << Post.find(post.post_id)
+      end
+      my_posts.each do |mp|
+        # byebug
+        bike_owner_post = {}
+        bike = Bike.find(mp.bike_id)
+        owner = User.find(mp.owner_id)
+        renter_post = RenterPost.find_by(post_id: mp.id)
+        bike_owner_post[:bike] = bike
+        bike_owner_post[:owner] = owner
+        bike_owner_post[:post] = mp
+        bike_owner_post[:renter_post] = renter_post
+        my_rented_bikes << bike_owner_post
+      end
+      render json: {my_rented_bikes: my_rented_bikes}
+    else
+      render json: { message: "Must be logged in" }
     end
-    my_renter_posts.each do |post|
-      my_posts << Post.find(post.post_id)
-    end
-    my_posts.each do |mp|
-      bike_and_owner = {}
-      bike = Bike.find(mp.bike_id)
-      owner = User.find(mp.owner_id)
-      bike_and_owner[:bike] = bike
-      bike_and_owner[:owner] = owner
-      my_rented_bikes << bike_and_owner
-    end
-    render json: { my_rented_bikes: my_rented_bikes, my_posts: my_posts, my_renter_posts: my_renter_posts }
   end
 end
 
