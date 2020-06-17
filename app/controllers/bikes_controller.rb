41eee1f37d04c0c19a10c5a9e5ffd710051d40f4
github username <github email address>
2020-06-17 18:07:16 -0500

    created my_bikes_history in bike controller for bike history

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,4 +1,5 @@
 class BikesController < ApplicationController
+    include CurrentUserConcern
 
     def index
         @bikes = Bike.all 
@@ -23,6 +24,41 @@ class BikesController < ApplicationController
             else
             # byebug
             render json: { status: 500 }
+        end
+    end
+
+    def my_bikes_history 
+        if @current_user
+            my_bikes = []
+            @my_bike_posts = {}
+            
+            # Get all bikes associated with current user
+            Bike.all.map do |bike|
+                if bike.owner_id == @current_user.id 
+                    my_bikes << bike
+                end
+            end
+            
+            #Iterate over my bikes and find posts associated with bike
+            my_bikes.each do |mb|
+                Post.all.map do |post|
+                    num = 0
+                    if post.bike_id == mb.id
+                        arr = []
+                        hash = {}
+                        hash[:bike] = mb
+                        hash[:post] = post 
+                        hash[:renter_post] = RenterPost.find_by(post_id: post.id)
+                        hash[:renter] = User.find(RenterPost.find_by(post_id: post.id).renter.id)
+                        arr << hash 
+                        @my_bike_posts[:"bike_#{num}"] = arr
+                    end
+                   num += 1
+                end
+            end
+            render json: @my_bike_posts
+        else
+            render json: { message: "Must be logged in" }
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
   get :bike_rent_history, to: "users#bike_rent_history"
+  get :my_bikes_history, to: "bikes#my_bikes_history"
   root to: "static#home"
   default_url_options :host => "localhost:3001"
 end
